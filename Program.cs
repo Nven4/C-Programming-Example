fcbc4b00fa5c33ea8c630fc2ca1545e4943e1e7e
Gnilitsky Veniamin <giveniamin21@list.ru>
2026-08-12 18:10:24 +0500

    add base structure

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Programming_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
