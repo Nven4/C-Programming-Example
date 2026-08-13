37bb6282cafcdcb2f882b242f4a6b494c3351034
Gnilitsky Veniamin <giveniamin21@list.ru>
2026-08-13 19:54:45 +0500

    update class

--- Degree.cs
+++ Degree.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace C_Programming_Example
 {
+    /// <summary>
+    /// Этот класс для нахождения градусов между сстрелками на часах
+    /// </summary>
     internal class Degree
     {
+        public int big { get; private set; } = null
+        public int small { get; private set; } = null
+
         int hour
         {
             get;
@@ -17,11 +23,19 @@ namespace C_Programming_Example
             init => field = value > 60 ? 60 : value;
         } = 0;
 
+        /// <summary>
+        /// Принимает стрелку часа на часах
+        /// </summary>
+        /// <param name="hour"></param>
         public Degree(int hour) : this(hour, 0)
         {
             this.hour = hour;
         }
-
+        /// <summary>
+        /// Принимает стрелку часа и стрелку минут
+        /// </summary>
+        /// <param name="hour"></param>
+        /// <param name="minute"></param>
         public Degree(int hour, int minute)
         {
             this.hour = hour;
