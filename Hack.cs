8bfb43d5cd613aa8c8e385bf1462d9a9f7b6e6d7
Gnilitsky Veniamin <giveniamin21@list.ru>
2026-08-22 13:23:16 +0500

    add full classs

--- Hack.cs
+++ Hack.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace C_Programming_Example
 {
-    internal class Hack
+    internal static class Hack
     {
+        static public void Start(string hackpassword, string password = "")
+        {
+            int counttry = 0;
+
+            char[] numbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            if (hackpassword == password)
+            {
+                Console.WriteLine("Найден за " + counttry + " попыток");
+            }
+            else
+            {
+                if (password.Length == hackpassword.Length)
+                {
+                    Console.WriteLine(password);
+                }
+                else
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        Start(hackpassword, password + numbers[i]);
+                        counttry++;
+                    }
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,12 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int hour = Convert.ToInt16(Console.ReadLine());
-            int minute = Convert.ToInt16(Console.ReadLine());
-
-            Degree degree = new Degree(hour, minute);
-
-            Console.WriteLine("Большее значение: " + degree.big + " " + "Меньшее значение: " + degree.small);
+            Hack.Start("SUP");
         }
     }
 }
