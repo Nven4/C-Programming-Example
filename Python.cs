06789db84442f32666403e892d075079e012b12f
Gnilitsky Veniamin <giveniamin21@list.ru>
2026-09-02 10:45:11 +0500

    update game matrix

--- Python.cs
+++ Python.cs
@@ -20,22 +20,33 @@ namespace C_Programming_Example
 
             field = new string[,]
             {
-                {"1|", "0", "0", "0", "0", "0"},
-                {"2|", "0", "0", "0", "0", "0"},
-                {"3|", "0", "0", "0", "0", "0"},
-                {"4|", "0", "0", "0", "0", "0"},
-                {"5|", "0", "0", "0", "0", "0"}
+                {"1|", "0", "0", "0", "0", "0", "|"},
+                {"2|", "0", "0", "0", "0", "0", "|"},
+                {"3|", "0", "0", "0", "0", "0", "|"},
+                {"4|", "0", "0", "0", "0", "0", "|"},
+                {"5|", "0", "0", "0", "0", "0", "|"}
             };
 
             for(int i = 0; i < field.GetLength(0); i++)
             {
+                int count = 0;
+
                 for(int j = 1; j < field.GetLength(1); j++)
                 {
-                    field[i, j] = Convert.ToString(random.Next(0, 2));
+                    if (field[i, j] == "|")
+                        continue;
+
+                    int rand = random.Next(0,2);
+
+                    if(rand == 1)
+                        count++;
+
+                    field[i, j] = Convert.ToString(rand);
                 }
+                field[i, 0] = Convert.ToString(count)+"|";
             }
 
-            Console.WriteLine("  12345");
+            GetHoryzCount(field);
 
             for (int i = 0;  i < field.GetLength(0); i++)
             {
@@ -46,6 +57,28 @@ namespace C_Programming_Example
 
                 Console.WriteLine();
             }
+        }
+
+        static void GetHoryzCount(string[,] field)
+        {
+            Console.Write("  ");
+
+            int count = 1;
+            int horyzcount = 0;
+            for(int i = 0; count < 6; i++)
+            {
+                if (i == 5)
+                {
+                    i = 0;
+                    count++;
+                    Console.Write(horyzcount);
+                    horyzcount = 0;
+                }
+
+                if (field[i, count] == "1")
+                    horyzcount++;
+            }
+            Console.WriteLine();
         }
     }
 }
